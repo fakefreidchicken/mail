80cc385816985ecd8c24aa03f88a374eb0d3d548
fakefreidchicken <529576863@qq.com>
2024-03-08 03:49:36 +0800

    项目初始结构

---
+++ mail-coupon/src/test/java/com/syx/mail/coupon/MailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syx.mail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
